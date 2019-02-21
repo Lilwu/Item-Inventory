ac043095f43e58e408af35d35fc894dd69f2272d
Lilwu <bboywu@gmail.com>
2019-02-21 20:57:23 +0800

    Item.cs 第一次提交 20190221

---
+++ Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public string ItemName;
+    public Sprite Icon;
+}
+
+public class ItemEventArgs: EventArgs
+{
+    public ItemEventArgs(Item item)
+    {
+        Item = item;
+    }
+
+    public Item Item;
+}
+

54e9f02bf2f5339e55d5b8b4ecb46b7e745c7268
Lilwu <bboywu@gmail.com>
2019-02-21 20:59:07 +0800

    RestorableItem.cs 第一次提交 20190221

---
+++ RestorableItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RestoreItemType
+{
+    Potion,
+    Scroll,
+}
+
+
+[CreateAssetMenu]
+public class RestorableItem : Item
+{
+    public int add_HP;       //回復血量
+    public int add_MP;       //回復魔力
+    public int add_Speed;    //增加速度
+    [Space]
+    public bool BackToTown;   //傳送回村莊
+
+    public RestoreItemType restoreItemType;
+}

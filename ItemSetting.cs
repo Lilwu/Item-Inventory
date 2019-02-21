96636bfc64501153845710d6e004075665883cd5
Lilwu <bboywu@gmail.com>
2019-02-21 20:58:09 +0800

    ItemSetting.cs 第一次提交 20190221

---
+++ ItemSetting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//給GameObject抓取Item.asset數值;
+public class ItemSetting : MonoBehaviour
+{
+    public EquippableItem setting;
+
+    private void Awake()
+    {
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<ItemBase>().OnPickUp();
+            FindObjectOfType<Inventory>().AddItem(setting);
+            Debug.Log("消除物件" + setting.ItemName);
+        }
+    }
+}

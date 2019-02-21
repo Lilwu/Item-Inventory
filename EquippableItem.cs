319918f882d63f007720f9f2a685182316148359
Lilwu <bboywu@gmail.com>
2019-02-21 20:56:06 +0800

    EquippableItem.cs 第一次提交 20190221

---
+++ EquippableItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EquipmentType
+{
+    Helmet,
+    Chest,
+    Gloves,
+    Boots,
+    Weapon,
+    Shield,
+    Accessory1,
+    Accessory2,
+}
+
+[CreateAssetMenu]
+public class EquippableItem : Item
+{
+    public int StrengthBonus;
+    public int AgilityBonus;
+    [Space]
+
+    public int Damge;      //傷害值
+    public int Defense;    //防禦力
+    public int Durability; //耐久度
+    [Space]
+    public int weight; //重量
+
+    public EquipmentType equipmentType;
+}

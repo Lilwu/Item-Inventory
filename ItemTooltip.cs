b96f11ccffc4c183f2170a3af1d6b80c7f296329
Lilwu <bboywu@gmail.com>
2019-02-21 20:58:48 +0800

    ItemTooltip.cs 第一次提交 20190221

---
+++ ItemTooltip.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemTooltip : MonoBehaviour
+{
+    [SerializeField] Text ItemNameText;
+    [SerializeField] Text ItemSlotText;
+    [SerializeField] Text ItemStatText;
+
+    private StringBuilder sb = new StringBuilder();
+
+    //裝備顯示
+    public void ShowTooltip(EquippableItem item)
+    {
+        ItemNameText.text = item.ItemName;
+        ItemSlotText.text = item.equipmentType.ToString();
+
+        sb.Length = 0;
+        AddStat(item.Damge, "攻擊力");
+        AddStat(item.Defense, "防禦力");
+        AddStat(item.weight, "重量");
+
+        //調整裝備顯示顏色(綠色)
+        ItemStatText.color = Color.green;
+        ItemStatText.text = sb.ToString();
+
+        gameObject.SetActive(true);   
+    }
+
+    //補助道具顯示 20190221
+    public void ShowTooltip(RestorableItem item)
+    {
+        ItemNameText.text = item.ItemName;
+        ItemSlotText.text = item.restoreItemType.ToString();
+
+        sb.Length = 0;
+        AddStat(item.add_HP, "玩家體力");
+        AddStat(item.add_MP, "玩家魔力");
+        AddStat(item.add_Speed, "攻擊速度");
+
+        //調整補助道具顯示顏色(紅色) 20190221
+        ItemStatText.color = Color.red;
+        ItemStatText.text = sb.ToString();
+
+        gameObject.SetActive(true);
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void AddStat(float value , string statName)
+    {
+        if(value != 0)
+        {
+            if (sb.Length > 0)
+                sb.AppendLine();
+
+
+            if (value > 0)
+                sb.Append("+");
+            sb.Append(value);
+            sb.Append(" ");
+            sb.Append(statName);
+        }
+
+    }
+}

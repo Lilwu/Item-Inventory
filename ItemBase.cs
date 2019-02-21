85c8411130b9b847ad2e3661ee88490494d0d85f
Lilwu <bboywu@gmail.com>
2019-02-21 20:57:45 +0800

    ItemBase.cs 第一次提交 20190221

---
+++ ItemBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{   
+
+    public void OnPickUp()
+    {
+        gameObject.SetActive(false);
+    }
+}

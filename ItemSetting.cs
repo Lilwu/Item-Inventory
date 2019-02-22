691f8284af35448692586d888a7fc6ad93606371
Lilwu <bboywu@gmail.com>
2019-02-23 00:22:22 +0800

    ItemSetting.cs 第三次提交：製作ItemID前備份 20190223

--- ItemSetting.cs
+++ ItemSetting.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 //給GameObject抓取Item.asset數值;
 public class ItemSetting : MonoBehaviour
 {
-    public EquippableItem setting;
+    [SerializeField] Inventory inventory;
+
+    public Item setting;
 
     private void Awake()
     {
+        inventory = FindObjectOfType<Inventory>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        if(Input.GetKeyDown(KeyCode.T))
         {
             GetComponent<ItemBase>().OnPickUp();
-            FindObjectOfType<Inventory>().AddItem(setting);
+            inventory.AddItem(setting);
             Debug.Log("消除物件" + setting.ItemName);
         }
     }

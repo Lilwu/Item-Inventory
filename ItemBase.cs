82b8e1843aa8d7d72beae621e1f886198853ef77
Lilwu <bboywu@gmail.com>
2019-02-23 00:22:00 +0800

    ItemBase.cs 第三次提交：製作ItemID前備份 20190223

--- ItemBase.cs
+++ ItemBase.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ItemBase : MonoBehaviour
-{   
+{
+    [SerializeField] Inventory inventory;
+
+    public event Action<ItemSlot> OnDropEvent;
+
+    private void OnValidate()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
 
     public void OnPickUp()
     {
